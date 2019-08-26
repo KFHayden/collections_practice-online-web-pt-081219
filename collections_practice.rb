e217185aea37530d002f6726839c7ac619872a5c
Kevin Hayden <kevin.fhayden@gmail.com>
2019-08-26 04:18:43 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,47 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length<=>b.length
+  end
+end
+
+def swap_elements(array)
+  #array_ashley = array[1]
+  #array[1] = array[2]
+  #array[2] = array_ashley
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  
+end
+
+def find_a
+  
+end
+
+def sum_array
+  
+end
+
+def add_s(array)
+  array.map.with_index do |word, index|
+    if index == 1
+      word
+    else
+      word << "s"
+    end
+  end
+end
